e7b34aa15e2017e73dd8a737499cf62c936d4854
Tobias <tobias.manger@swisscom.com>
2017-05-01 14:45:28 +0200

    add some more assets to the game

--- src/assets.ts
+++ src/assets.ts
@@ -1,10 +1,91 @@
 /* AUTO GENERATED FILE. DO NOT MODIFY. YOU WILL LOSE YOUR CHANGES ON BUILD. */
 
 export namespace Images {
+    export class ImagesGamerounds {
+        static getName(): string {
+            return 'Gamerounds';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/Gamerounds.png');
+        };
+    }
+    export class ImagesInfluence {
+        static getName(): string {
+            return 'Influence';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/Influence.png');
+        };
+    }
     export class ImagesMap {
         static getName(): string { return 'map'; };
 
         static getPNG(): string { return require('assets/images/map.png'); };
+    }
+    export class ImagesMapSmall {
+        static getName(): string {
+            return 'mapSmall';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/mapSmall.png');
+        };
+    }
+    export class ImagesMenubackground {
+        static getName(): string {
+            return 'menubackground';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/menubackground.png');
+        };
+    }
+    export class ImagesMenuInvluence {
+        static getName(): string {
+            return 'MenuInvluence';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/MenuInvluence.png');
+        };
+    }
+    export class ImagesMenuRounds {
+        static getName(): string {
+            return 'MenuRounds';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/MenuRounds.png');
+        };
+    }
+    export class ImagesMenuSupply {
+        static getName(): string {
+            return 'MenuSupply';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/MenuSupply.png');
+        };
+    }
+    export class ImagesMenuVictory {
+        static getName(): string {
+            return 'MenuVictory';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/MenuVictory.png');
+        };
+    }
+    export class ImagesMenuWildlings {
+        static getName(): string {
+            return 'MenuWildlings';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/MenuWildlings.png');
+        };
     }
     export class ImagesOrderTokenFront {
         static getName(): string {
@@ -15,10 +96,64 @@ export namespace Images {
             return require('assets/images/orderTokenFront.png');
         };
     }
+    export class ImagesOrderTokenFront45 {
+        static getName(): string {
+            return 'orderTokenFront45';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/orderTokenFront45.png');
+        };
+    }
     export class ImagesOrdertokens {
         static getName(): string { return 'ordertokens'; };
 
         static getPNG(): string { return require('assets/images/ordertokens.png'); };
+    }
+    export class ImagesOrdertokens45 {
+        static getName(): string {
+            return 'ordertokens45';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/ordertokens45.png');
+        };
+    }
+    export class ImagesSupply {
+        static getName(): string {
+            return 'Supply';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/Supply.png');
+        };
+    }
+    export class ImagesVictory {
+        static getName(): string {
+            return 'Victory';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/Victory.png');
+        };
+    }
+    export class ImagesWildlingCardCover {
+        static getName(): string {
+            return 'WildlingCardCover';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/WildlingCardCover.png');
+        };
+    }
+    export class ImagesWildlingStatus {
+        static getName(): string {
+            return 'WildlingStatus';
+        };
+
+        static getPNG(): string {
+            return require('assets/images/WildlingStatus.png');
+        };
     }
 }
 
--- src/states/game.ts
+++ src/states/game.ts
@@ -1,34 +1,47 @@
-import {OrderTokenService} from "../others/orderToken";
-import {BoardService} from "../others/board";
+import {OrderTokenService} from "../ui/orderToken";
+import {Board} from "../ui/board";
+import {TopMenu} from "../ui/topMenu";
+
 import game = PIXI.game;
 
 export default class Game extends Phaser.State {
     private orderTokenService: OrderTokenService;
-    private boardService: BoardService;
+    private boardService: Board;
+    private topMenu: TopMenu;
+    private currentGameWidth: number;
 
     constructor() {
         super();
         this.orderTokenService = new OrderTokenService();
-        this.boardService = new BoardService();
+        this.boardService = new Board();
+        this.topMenu = new TopMenu();
     }
 
     public preload() {
-        BoardService.loadAssets(this.game);
+        Board.loadAssets(this.game);
         this.orderTokenService.loadAssets(this.game);
+        this.topMenu.loadAssets(this.game);
 
     }
 
     public create(): void {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        BoardService.createBoard(this.game);
+        Board.createBoard(this.game);
         this.orderTokenService.addPlanningLayer(this.game);
+        this.topMenu.draw(this.game);
         this.orderTokenService.creatOrderTokens(this.game);
         this.game.input.enabled = true;
+        this.currentGameWidth = window.innerWidth;
     }
 
     public update() {
         this.boardService.handleNavigationOnMap(this.game);
         this.boardService.handleZoom(this.game);
+        if (this.currentGameWidth != window.innerWidth) {
+            this.topMenu.draw(this.game);
+            this.currentGameWidth = window.innerWidth;
+        }
+
     }
 
 }
